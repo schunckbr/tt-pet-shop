ff537e967770ccb98b5a6fdb9a95d996c0b42983
cassio <cassio@gmail.com>
2015-11-28 17:18:52 -0200

    salvando proprietario

--- src/main/java/br/com/tt/web/pet/controller/bean/ProprietarioBean.java
+++ src/main/java/br/com/tt/web/pet/controller/bean/ProprietarioBean.java
@@ -1,8 +1,11 @@
 package br.com.tt.web.pet.controller.bean;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+import br.com.tt.web.pet.dao.Dao;
 import br.com.tt.web.pet.model.Proprietario;
 
 @ManagedBean
@@ -10,6 +13,8 @@ import br.com.tt.web.pet.model.Proprietario;
 public class ProprietarioBean {
 
 	private Proprietario proprietario = new Proprietario();
+	private Dao dao = new Dao();
+	private List<Proprietario> lista = dao.buscarTodos(proprietario);
 
 	public Proprietario getProprietario() {
 		return proprietario;
@@ -22,6 +27,8 @@ public class ProprietarioBean {
 	public void salvar() {
 		System.out.println("Salvando com sucesso!");
 		System.out.println(proprietario.getCpf());
+		dao.salvar(proprietario);
+		proprietario = new Proprietario();
 	}
 
 }
--- src/main/java/br/com/tt/web/pet/model/Proprietario.java
+++ src/main/java/br/com/tt/web/pet/model/Proprietario.java
@@ -2,13 +2,38 @@ package br.com.tt.web.pet.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
 public class Proprietario implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@SequenceGenerator(name = "proprietario_seq", 
+						sequenceName = "proprietario_seq", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "proprietario_seq")
+	private Long id;
+	
 	private String nome;
 	private String cpf;
 	private String telefone;
 	private String email;
+	
+	
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getNome() {
 		return nome;
